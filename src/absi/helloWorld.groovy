e2a27bef0dc4c1b7ec6a673510f384ebc2375d1e
dockerman2020 <75863443+dockerman2020@users.noreply.github.com>
2023-12-17 12:06:55 +0000

    Updated step to call sharedlibrary class

--- src/absi/helloWorld.groovy
+++ src/absi/helloWorld.groovy
@@ -1,4 +1,4 @@
-package test.sharedlibrary.absi
+package src.absi.test
 
 def helloWorld() {
     echo "SharedLibrary, World! This is from the shared library."
--- var/SharedLibrary.groovy
+++ var/SharedLibrary.groovy
@@ -1,4 +1,4 @@
-package test.sharedlibrary.absi
+
 
 def helloWorld() {
     echo "SharedLibrary, World! This is from the shared library."
